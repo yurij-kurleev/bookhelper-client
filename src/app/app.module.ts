193183196d291da4e62be540cbe59ed9d4ebb334
89356 <ykurleev88@gmail.com>
2017-05-28 13:19:58 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AuthorizeComponent } from './authorize/authorize.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthorizeComponent
   ],
   imports: [
     BrowserModule,
